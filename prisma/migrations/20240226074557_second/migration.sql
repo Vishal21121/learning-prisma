65c0d49b4105e21e442ec61b3f2d15370f47bf78
Vishal21121 <vs84077000@gmail.com>
2024-02-26 14:57:39 +0530

    added code for reading and creating data in the database

---
+++ prisma/migrations/20240226074557_second/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `user` MODIFY `largeNumber` BIGINT NULL,
+    MODIFY `prefereces` JSON NULL;
--- script.ts
+++ script.ts
@@ -1,10 +1,84 @@
 import { PrismaClient } from '@prisma/client'
+//* we can find what query is being run behind the scenes using the below syntax
+// const prisma = new PrismaClient({ log: ['query'] })
+
+
 const prisma = new PrismaClient()
 
 async function main() {
-    // const user = await prisma.user.create({ data: { name: "shubham" } })
-    const user = await prisma.user.deleteMany()
-    console.log(user)
+    // await prisma.user.deleteMany()
+
+    //* CREATE OPERATION
+    // const user = await prisma.user.create({
+    //     data: {
+    //         name: "vishal",
+    //         email: "vishal@gmail.com",
+    //         age: 21,
+    //         // for creating a field related to another table we need to perform the below operation.
+    //         UserPreference: {
+    //             create: {
+    //                 emailUpdates: true
+    //             }
+    //         },
+    //     },
+    //     //* for getting the field which is related to another table we need to do this in the below way.
+    //     // include: {
+    //     //     UserPreference: true
+    //     // }
+
+    //     //* we can use select as well to select specific details
+    //     //* But we can use either select or include
+    //     select: {
+    //         name: true,
+    //         UserPreference: {
+    //             select: {
+    //                 id: true
+    //             }
+    //         }
+    //     }
+    // })
+
+    // const users = await prisma.user.createMany({
+    //     data: [
+    //         {
+    //             name: "vishal singh",
+    //             age: 21,
+    //             email: "one@gmail.com"
+    //         },
+    //         {
+    //             name: "vishal singh",
+    //             age: 22,
+    //             email: "two@gmail.com"
+    //         },
+    //     ]
+    // })
+
+    // READ OPERATION
+    // const user = await prisma.user.findUnique({
+    //     where: {
+    //         email: "one@gmail.com"
+    //     }
+    //* here we can use select or include as discussed in create operation
+    // })
+
+    // const user = await prisma.user.findFirst({
+    //     where: {
+    //         name: "vishal singh"
+    //     }
+    // })
+
+    const user = await prisma.user.findMany({
+        where: {
+            name: "vishal singh"
+        },
+        // distinct: ["name"]
+        //* we can achieve pagination using take and skip
+        //* take is for taking the number of values and skip is for skipping the values.
+        take: 2,
+        skip: 1
+    })
+
+    console.log("found user details", user)
 }
 
 main().catch(e => {
